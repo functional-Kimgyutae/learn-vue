8391f63fe231eb98b89811a7d6c6816340e7c713
functional-Kimgyutae <stoprabbit2004@gmail.com>
2022-07-06 09:06:38 +0900

    .

--- vue-cli/src/a.vue
+++ vue-cli/src/a.vue
@@ -1,10 +1,16 @@
 <template>
-  
+    <!-- html -->
+  <div>header</div>
 </template>
 
 <script>
 export default {
+    methods: {
+        //Javascript - 인스턴스 옵션
+        addNum: function() {
 
+        }
+    }
 }
 </script>
 
