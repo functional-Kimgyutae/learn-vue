d20f3249ccce6d5d01f3118e5db40f705e51f6d8
functional-Kimgyutae <stoprabbit2004@gmail.com>
2022-09-16 14:34:21 +0900

    Update JobsView.vue

--- vue-news/src/views/JobsView.vue
+++ vue-news/src/views/JobsView.vue
@@ -29,30 +29,5 @@ export default {
   </script>
   
   <style scoped>
-    .news-list {
-        margin: 0;
-        padding: 0;
-    }
-    
-    .post {
-        list-style: none;
-        display: flex;
-        align-items: center;
-        border-bottom: 1px solid #eee;
-    }
-    
-    .points {
-        width: 80px;
-        height: 60px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        color:#42b883;
-    }
-    .news-title {
-        margin: 0;
-    }
-    .link-text {
-        color: #828282;
-    }
+
     </style>
